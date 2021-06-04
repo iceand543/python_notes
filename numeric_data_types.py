1dd211117ff1231c21842ea56b16be437afb59b2
iceand543 <viswatrendy@gmail.com>
2021-06-04 01:05:28 -0400

    3rd day notes

---
+++ numeric_data_types.py
@@ -0,0 +1,26 @@
+# Python 3 data types are int, float, complex
+
+# int doesn't have a size limit
+# float example is 12.2
+# int is considerablly less process intense than float.
+
+#operators examples
+
+a = 12
+b = 3
+c = 15.5 # float example Adn you can't use float values as range in python. for i in range (1,4) # not range(1, 4.0)
+
+print(a / b) # a/b si called the expression. Expression is anything that evaluates something. even a = 10 is an expression
+print(a // b)
+print(a % b)
+
+# anything that python need to figure out is an expression. even simple print(a) is an expression as python -
+# need to figure out the value of a
+# in a = 2 a is not an expression as a is the just bound-to value
+# another example is for i in range(1,4), i is not an expression as a is the just bound-to value
+
+# Operator precedence means, math have priority of operators while performing calculation on an with /,*,+,-
+# Operator precedence acronym example is PEMDAS - peranthesis, exponentials, multiplication/division, add/sub or BADMAS
+# Multi and Div have same precedence. In a same way add/sub have same precedence.
+# you need to calculate from left to right if you encounter any two of those with same precedence
+
--- strings.py
+++ strings.py
@@ -9,9 +9,30 @@ name = "laks"
 
 print(greeting+ ' ' +name)
 
-input_name = input("enter your name")
-age = input("what's your age")
-print(input_name + "'s age is",age)
+# input_name = input("enter your name")
+# age = input("what's your age")
+# print(input_name + "'s age is",age)
 # when you use comma, it just acts as a space.
 # Use + instead to concatenate.
 # literal is any value of type. For example, "text sample", 6 are literals of different data types
+# String data type can be called a sequence type
+
+#------------------------------------------------------------------------------------------------------------
+#string index
+parrot = "some sameple text"
+print(parrot[1])
+print(parrot[-1])
+
+# string slicing
+print(parrot[1:4]) # it would slice upto index 3 but not include 4
+print(parrot[:4]) # prints from 0 index
+print(parrot[1:]) # prints until last index
+print(parrot[-4:-2])
+
+# string step
+print(parrot[1:6:2])# two here is the interval
+number = "1,234,543:324,234;234.32"
+separators = number[1::4]
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val) for val in values])
+# using negative step value, you can print the string backwards
